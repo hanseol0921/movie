4386df268eb02df947b8239ff13bb2731db36d1c
lny <leenayoungmzzu012@gmail.com>
2025-12-14 03:34:01 +0900

    251214

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ KOBIS_API_KEY = '0dfd8752d1b4b76ed1d45011c6607d56'  # 영화진흥위원회 API 
 NAVER_CLIENT_ID = 'm6nZpyW187lm1c7iMKSH'  # 네이버 API 클라이언트 ID
 NAVER_CLIENT_SECRET = '6yXrem4rjM'  # 네이버 API 시크릿
 
+
 @app.route('/')
 def index():
     return render_template('main.html')
@@ -170,15 +171,75 @@ def get_movie_poster():
     }
     params = {
         'query': movie_name,
-        'display': 1
+        'display': 5  # 여러 결과를 가져와서 매칭
     }
     
     try:
         response = requests.get(url, headers=headers, params=params)
         data = response.json()
+        
+        # 영화 제목이 가장 일치하는 것 찾기
+        if data.get('items'):
+            best_match = None
+            for item in data['items']:
+                title = item['title'].replace('<b>', '').replace('</b>', '')
+                if movie_name in title or title in movie_name:
+                    best_match = item
+                    break
+            
+            if not best_match:
+                best_match = data['items'][0]
+            
+            return jsonify({'items': [best_match]})
+        
         return jsonify(data)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+
+@app.route('/api/search-boxoffice-all')
+def search_boxoffice_all():
+    movie_name = request.args.get('name', '')
+    
+    # 최근 30일간 검색
+    from datetime import datetime, timedelta
+    
+    results = []
+    found = False
+    
+    # 어제부터 30일 전까지 검색
+    for i in range(30):
+        target_date = datetime.now() - timedelta(days=i+1)
+        formatted_date = target_date.strftime('%Y%m%d')
+        
+        url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json'
+        params = {
+            'key': KOBIS_API_KEY,
+            'targetDt': formatted_date
+        }
+        
+        try:
+            response = requests.get(url, params=params)
+            data = response.json()
+            
+            movie_list = data.get('boxOfficeResult', {}).get('dailyBoxOfficeList', [])
+            
+            for movie in movie_list:
+                if movie_name.lower() in movie['movieNm'].lower() or movie['movieNm'].lower() in movie_name.lower():
+                    movie['searchDate'] = target_date.strftime('%Y-%m-%d')
+                    results.append(movie)
+                    found = True
+                    break
+            
+            if found:
+                break
+                
+        except Exception as e:
+            continue
+    
+    if results:
+        return jsonify({'boxoffice': results[0], 'found': True})
+    else:
+        return jsonify({'found': False, 'error': '최근 30일 내 박스오피스에서 해당 영화를 찾을 수 없습니다.'})
 
 if __name__ == '__main__':
     app.run(debug=True)
